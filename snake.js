b1ca473b0cca78d8e441568394187a19ef7db2db
Nasser Atiyeh <nasseratiyeh@gmail.com>
2019-10-23 16:23:58 -0500

    image, snake, food create

--- snake.js
+++ snake.js
@@ -1,1 +1,25 @@
-var PORT = process.env.PORT || 5000;
+const cvs = document.getElementById("snake");
+const ctx = cvs.getContext("2d");
+
+// unit
+const box = 32;
+
+// image
+const ground = new Image();
+ground.src = "img/ground.png";
+
+const foodImg = new Image();
+foodImg.src = "img/food.png";
+
+// snake
+let snake = [];
+snake[0] = {
+  x : 9 * box,
+  y : 10 * box
+}
+
+// food
+let food = {
+  x : Math.floor(Math.random()*17+1) * box,
+  y : Math.floor(Math.random()*15+3) * box,
+}
